be976f2655b54170643e2322ba67b81ebfe8c343
James Bourne <jamesbourne@outlook.com>
2021-07-23 12:01:26 +0200

    tweak setup scripts and bump version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM cockroachdb/cockroach:v21.1.2
+FROM cockroachdb/cockroach:v21.1.6
 
 ADD init_cluster.sh /cockroach/
 ADD start_fly.sh /cockroach/
+
+CMD [ "shell", "/cockroach/start_fly.sh" ]
--- start_fly.sh
+++ start_fly.sh
@@ -2,14 +2,22 @@
 
 set -eu
 
-echo "Starting on Fly ..."
+echo "Starting on Cockroach DB on Fly ..."
 echo exec /cockroach/cockroach start \
   --insecure \
-  --locality=fly_region=$FLY_REGION \
+  --logtostderr \
+  --locality=region=$FLY_REGION \
+  --max-sql-memory 64MiB \
+  --cache 64MiB \
   --cluster-name=$FLY_APP_NAME \
-  --join=$FLY_APP_NAME.internal
+  --join=fra.$FLY_APP_NAME.internal,cdg.$FLY_APP_NAME.internal,ams.$FLY_APP_NAME.internal,lhr.$FLY_APP_NAME.internal \
+  --advertise-addr=$FLY_REGION.$FLY_APP_NAME.internal
 exec /cockroach/cockroach start \
   --insecure \
-  --locality=fly_region=$FLY_REGION \
+  --logtostderr \
+  --locality=region=$FLY_REGION \
+  --max-sql-memory 64MiB \
+  --cache 64MiB \
   --cluster-name=$FLY_APP_NAME \
-  --join=$FLY_APP_NAME.internal
+  --join=fra.$FLY_APP_NAME.internal,cdg.$FLY_APP_NAME.internal,ams.$FLY_APP_NAME.internal,lhr.$FLY_APP_NAME.internal \
+  --advertise-addr=$FLY_REGION.$FLY_APP_NAME.internal
